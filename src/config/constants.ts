f1e9fcc3e40fa77fb81360a1c6be9fe190aeae59
Marielly Blank <84345216+mariellyblank@users.noreply.github.com>
2022-06-25 22:28:44 +0100

    Delete src directory

--- src/config/constants.ts
+++
@@ -1,5 +0,0 @@
-export const config = {
-	API_URL: 'http://localhost:5000',
-	MONGO_CONNECTION:
-		'mongodb+srv://admin:<password>@url-shortner-dio.9ywemus.mongodb.net/?retryWrites=true&w=majority',
-}
